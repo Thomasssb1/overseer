ef7932a8cf08860291e7fb03311a78c58766304a
Thomasssb1 <thomas.beer04@outlook.com>
2026-03-21 02:56:15 +0000

    Add runner export

--- lib/overseer.dart
+++ lib/overseer.dart
@@ -21,11 +21,10 @@
 ///   await wrapper.run();
 /// }
 /// ```
-library overseer;
-
 export 'src/exceptions.dart';
 export 'src/execution_wrapper/execution_wrapper.dart'
     show ExecutionWrapper, ArtifactGenerator;
 export 'src/matrix_engine/matrix_engine.dart' show MatrixEngine;
 export 'src/matrix_engine/matrix_mode.dart' show MatrixMode;
 export 'src/models.dart';
+export 'src/overseer_runner.dart' show OverseerRunner;
